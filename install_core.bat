ea9ab6e6557cbac45175f73e7d9105328884c43e
David Miller <davidmillercn@outlook.com>
2024-01-17 22:14:34 +0800

    v2.0

--- install_core.bat
+++
@@ -1,5 +0,0 @@
-%Windir%\System32\certutil.exe -addstore CA R4_R1RootCA.crt
-%Windir%\System32\certutil.exe -addstore CA R4_R2RootCA.crt
-%Windir%\System32\certutil.exe -addstore CA R4_R3RootCA.crt
-regedit.exe /s R4RootCA.reg
-regedit.exe /s R4_RootCertificateAuthority.reg
