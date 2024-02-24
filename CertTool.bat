743743a973ef6965a3b2bd7e70fb03e50724a397
David Miller <admin@davidmiller.top>
2024-02-24 16:23:58 +0800

    v2.4.0.3

--- CertTool.bat
+++ CertTool.bat
@@ -1004,7 +1004,7 @@ goto invalidOption
 echo Finished!
 echo Author: David Miller Trust Services Team
 echo Website: https://go.davidmiller.top/pki
-echo Version 2.4 ^(Pre-release Build 2^)
+echo Version 2.4 ^(Pre-release Build 3^)
 goto loopChoice
 
 :loopChoice
@@ -1018,12 +1018,12 @@ if not defined loopOption (
 if %loopOption%==1 (
 	cls
 	set echoName=true
-	set installFailedOption=
+	set loopOption=
 	goto choice
 )
 if %loopOption%==2 (
 	exit
 )
 set choice=loop
-set installFailedOption=
+set loopOption=
 goto invalidOption
